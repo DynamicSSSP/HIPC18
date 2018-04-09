558f655679913a9ca0aa4bf8336792e2604b7322
DynamicSSSP <37888745+DynamicSSSP@users.noreply.github.com>
2018-04-09 10:12:42 -0400

    Update run_spark.sh

--- run_spark.sh
+++ run_spark.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
+
 $SPARK_HOME/bin/spark-submit --packages com.databricks:spark-csv_2.10:1.3.0,it.unipd.dei:graphx-diameter_2.10:0.1.0 --class $2 --executor-cores 20 --driver-memory 15G --executor-memory 120G --conf "spark.akka.frameSize=2047" --master spark://$1:7077 sssp.jar $3 $4 
