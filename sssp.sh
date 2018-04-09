c6693cce9857d9ad162ddd1cca4204dfda47c397
DynamicSSSP <37888745+DynamicSSSP@users.noreply.github.com>
2018-04-09 02:14:44 -0400

    Create sssp.sh

---
+++ sssp.sh
@@ -0,0 +1,8 @@
+hostname=$1
+inputGraph=$2
+outputSP=$3
+log=$4
+addedEdges=$5
+num_iter=50
+source_vertex=8
+./run_spark.sh $hostname graph.SSSPIncAll $inputGraph $outputSP $log $addedEdges $num_iter $source_vertex
