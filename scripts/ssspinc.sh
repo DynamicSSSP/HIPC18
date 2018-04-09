d6fc13a287825d3ff2627c61ac3c78a4405386b3
DynamicSSSP <37888745+DynamicSSSP@users.noreply.github.com>
2018-04-09 11:45:34 -0400

    Update ssspinc.sh

--- scripts/ssspinc.sh
+++ scripts/ssspinc.sh
@@ -7,4 +7,4 @@ log=$4
 addedEdges=$5
 num_iter=50
 source_vertex=8
-./run_spark_app.sh $hostname inc.SSSPIncAll $inputGraph $outputSP $log $addedEdges $num_iter $source_vertex
+bash run_spark_app.sh $hostname inc.SSSPIncAll $inputGraph $outputSP $log $addedEdges $num_iter $source_vertex
