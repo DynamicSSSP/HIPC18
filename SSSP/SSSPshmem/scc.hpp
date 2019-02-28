e5c4f2720ee29cc6a4eda169da6aab9d3e3f5fb7
root <sriramsrinivas@unomaha.edu>
2019-02-28 03:00:34 -0600

    ssc.hpp

---
+++ SSSP/SSSPshmem/scc.hpp
@@ -0,0 +1,12 @@
+//
+// Created by root on 2/28/19.
+//
+
+#ifndef SSSP_SCC_HPP
+#define SSSP_SCC_HPP
+
+void update_SCC(A_Network *X, vector<SCC_vertex>*SCC,vector<xEdge> *allChange)
+{
+
+}
+#endif //SSSP_SCC_H
